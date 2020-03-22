eb3411eccfcaf947a9bf6bae07ea0b50300e7ec2
Karel Renaldi <karelrenaldi8@gmail.com>
2020-03-22 19:01:17 +0700

    Make interactive

--- assets/canvas.js
+++ assets/canvas.js
@@ -1,25 +1,35 @@
 const canvas = document.querySelector('canvas');
+const circleArray = [];
+const mouse = {
+    x:undefined,
+    y:undefined
+};
+const maxRadius = 50;
+const minRadius = 10;
+const colorArray = ["#f35588","#05dfd7","#a3f7bf","#fff591"];
+
+window.addEventListener('mousemove',(e)=>{
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
 // Setting width and height
 canvas.width  = window.innerWidth;
 canvas.height = window.outerHeight;
 // make context
 const c = canvas.getContext("2d");
 
-function Circle(x, y, dx, dy, radius){
+function Circle(x, y, dx, dy, radius, color){
     this.x  = x;
     this.y  = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.color = color;
     this.draw = ()=>{
-        const r = Math.random()*30;
-        const g = Math.random()*30;
-        const b = Math.random()*30;
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-        c.fillStyle = `rgba(33,36,61,0.5)`;
-        c.strokeStyle = `rgba(33,36,61,0.5)`;
-        c.stroke();
+        c.fillStyle = this.color;
         c.fill();
     }
     this.update = ()=>{
@@ -32,19 +42,26 @@ function Circle(x, y, dx, dy, radius){
         };
         this.x += this.dx;
         this.y += this.dy;
-    }
-}
+        // Interactive
+        if(mouse.x-this.x < 50 && mouse.y-this.y < 50 &&mouse.x-this.x > -50 && mouse.y-this.y > -50){
+            if(this.radius < maxRadius){
+                this.radius += 1;
+            };
+        }else if(this.radius > minRadius){
+            this.radius -= 1;
+        }
+    };
+};
 
 
-const circleArray = [];
-
-for(let i=0; i<80; i++){
-    let radius = 40;
+for(let i=0; i<120; i++){
+    let radius = minRadius;
     let x  = Math.random()*(innerWidth-2*radius)+radius;
     let y  = Math.random()*(innerHeight-2*radius)+radius;
     let dx = (Math.random()-0.5);
     let dy = (Math.random()-0.5);
-    circleArray.push(new Circle(x, y, dx, dy, radius));
+    let color = colorArray[Math.floor(Math.random()*colorArray.length)];
+    circleArray.push(new Circle(x, y, dx, dy, radius, color));
 }
 
 function animate(){
